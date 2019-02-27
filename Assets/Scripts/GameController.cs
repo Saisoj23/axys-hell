88bbc0a3d88ca07c6ba0a1033d7cdee75824f6cb
Saisoj23 <30736357+Saisoj23@users.noreply.github.com>
2019-02-27 13:54:30 -0300

    Simple attack editor

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,30 +15,84 @@ public class GameController : MonoBehaviour
     public enum Positions {Right, Left, Up, Down, Down_Right, Down_Left, Up_Right, Up_Left}
 
     public SpawnList spawns;
+    public AttackList attack;
+
+    public float timeBetweenAttacks;
+    public int indexToTest = -1;
+
+    bool gamePlaying = false;
 
 
     void Start()
     {
         filePath = Application.dataPath + "/JsonAttacks/4SideAttacks.json";
         jsonString = File.ReadAllText(filePath);
-        spawns = JsonUtility.FromJson<SpawnList>(jsonString);
-        //jsonString = JsonUtility.ToJson(spawns);
-        //File.WriteAllText(filePath, jsonString);
-        
-        StartCoroutine("Spawner");
+        attack = JsonUtility.FromJson<AttackList>(jsonString);
+        StartRandomAttacks();
     }
 
     IEnumerator Spawner ()
     {
-        foreach (Spawn i in spawns.spawns)
+        if (spawns.spawns.Length > 0)
         {
-            int intArrow = (int)i.arrow;
-            int intPosition = (int)i.position;
-            ArrowController arrow = Instantiate(arrows[intArrow], positions[intPosition], Quaternion.identity).GetComponent<ArrowController>();
-            arrow.speed = i.speed;
-            arrow.secondSpeed = i.secondSpeed;
-            if (i.spawnTime > 0) yield return new WaitForSeconds(i.spawnTime);
+            foreach (Spawn i in spawns.spawns)
+            {
+                int intArrow = (int)i.arrow;
+                int intPosition = (int)i.position;
+                ArrowController arrow = Instantiate(arrows[intArrow], positions[intPosition], Quaternion.identity).GetComponent<ArrowController>();
+                arrow.speed = i.speed;
+                arrow.secondSpeed = i.secondSpeed;
+                if (i.spawnTime > 0) yield return new WaitForSeconds(i.spawnTime);
+            }
         }
+    }
+
+    IEnumerator Attack ()
+    {
+        while (gamePlaying && attack.attacks.Count > 0)
+        {
+            if (indexToTest >= 0 && indexToTest < attack.attacks.Count) spawns = attack.attacks[indexToTest];
+            else spawns = attack.attacks[Random.Range(0, attack.attacks.Count)];
+            foreach (Spawn i in spawns.spawns)
+            {
+                int intArrow = (int)i.arrow;
+                int intPosition = (int)i.position;
+                ArrowController arrow = Instantiate(arrows[intArrow], positions[intPosition], Quaternion.identity).GetComponent<ArrowController>();
+                arrow.speed = i.speed;
+                arrow.secondSpeed = i.secondSpeed;
+                if (i.spawnTime > 0) yield return new WaitForSeconds(i.spawnTime);
+            }
+            yield return new WaitForSeconds(timeBetweenAttacks);
+        }
+    }
+
+    public void OverrideJson ()
+    {
+        jsonString = JsonUtility.ToJson(attack);
+        File.WriteAllText(filePath, jsonString);
+    }
+
+    public void AddAttackToList ()
+    {
+        attack.attacks.Add(spawns);
+    }
+
+    public void StartRandomAttacks ()
+    {
+        if (gamePlaying)
+        {
+            gamePlaying = false;
+        }
+        else 
+        {
+            gamePlaying = true;
+            StartCoroutine("Attack");
+        }
+    }
+
+    public void TestActualAttack ()
+    {
+        StartCoroutine("Spawner");
     }
 
     [System.Serializable]
@@ -55,5 +109,11 @@ public class GameController : MonoBehaviour
     public struct SpawnList
     {
         public Spawn[] spawns;
+    }
+
+    [System.Serializable]
+    public struct  AttackList
+    {
+        public List<SpawnList> attacks;
     }
 }
