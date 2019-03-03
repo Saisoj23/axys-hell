699e35baed337cc37b36ea52e37b5c7f3afe1878
Saisoj23 <30736357+Saisoj23@users.noreply.github.com>
2019-03-03 09:51:40 -0300

    New smart arrow sistem

--- Assets/Scripts/arrows/SmartArrowController.cs
+++ Assets/Scripts/arrows/SmartArrowController.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 
 public class SmartArrowController : ArrowController
 {
+    [Header("Special Values")]
+    public float smartDistance;
+    public Color lookingColor;
+
     bool visible = false;
+
+    SpriteRenderer sprite;
+    LineRenderer line; 
+
+    override protected void Awake ()
+    {
+        base.Awake();
+        sprite = GetComponent<SpriteRenderer>();
+        line = GetComponent<LineRenderer>();
+    }
+
     protected override IEnumerator MoveTo ()
     {
+        Vector2 target = rb.position.normalized * smartDistance;
+        while (rb.position != target)
+        {
+            rb.MovePosition(Vector2.MoveTowards(rb.position, target, speed * Time.deltaTime));
+            yield return null;
+        }
         bool looking = false;
+        line.enabled = true;
+        Color startColor = sprite.color;
         while (rb.position != Vector2.zero)
         {
+            line.SetPositions(new Vector3[] {transform.position, new Vector3(0f, 0f, 0.11f)});
             RaycastHit2D hit = Physics2D.Raycast(rb.position -rb.position.normalized * 0.2f, -rb.position.normalized);
             looking = hit.collider.CompareTag("Shield");
-            if (!visible || (visible && !looking))
+            if (looking)
             {
+                line.startColor = lookingColor;
+                line.endColor = lookingColor;
+                sprite.color = lookingColor;
+            }
+            else
+            {
+                line.startColor = startColor;
+                line.endColor = startColor;
+                sprite.color = startColor;
                 rb.MovePosition(Vector2.MoveTowards(rb.position, Vector2.zero, speed * Time.deltaTime));
             }
             yield return null;
         }
     }
-
-    void OnBecameVisible ()
-        {
-            visible = true;
-        }
 }
